97bd6ff1291fdd30f661aa434556685da42e009c
Tommi Ratamaa <tommi.ratamaa@gmail.com>
2013-11-29 17:56:18 +0200

    Vielä jatkoa edelliseen.

--- osoitepalvelu-ui/src/main/webapp/js/project.js
+++ osoitepalvelu-ui/src/main/webapp/js/project.js
@@ -1,21 +1,17 @@
 
-angular.module('OsoiteKoostepalvelu', []
-//    ,
-//    ['ngRoute']
-    )
-//	.config(function($routeProvider) {
-//		$routeProvider
-//		.when('/', {
-//			controller:'SearchController',
-//			templateUrl:'index.html'
-//		})
-//		.otherwise({
-//			redirectTo:'/'
-//		});
-//	})
-	.factory('Results', function() {
-		return [
-			{firstName: "Dummy", lastName: "Result"}
-		];
-	});
-
+angular.module('OsoiteKoostepalvelu', ['ngRoute'])
+        .config(function($routeProvider) {
+                $routeProvider
+                .when('/', {
+                        controller:'SearchController',
+                        templateUrl:'index.html'
+                })
+                .otherwise({
+                        redirectTo:'/'
+                });
+        })
+        .factory('Results', function() {
+                return [
+                        {firstName: "Dummy", lastName: "Result"}
+                ];
+        });
