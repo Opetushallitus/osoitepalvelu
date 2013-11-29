a3f3d665822a2df5dd6f36ae3a0b34722486599c
Tommi Ratamaa <tommi.ratamaa@gmail.com>
2013-11-29 17:50:31 +0200

    Poistettu turhat Springiin liittyvät tiedostot UI-projektista ja lisätty JavaScriptin minimointi POM:iin.

--- osoitepalvelu-ui/src/main/webapp/js/project.js
+++ osoitepalvelu-ui/src/main/webapp/js/project.js
@@ -1,15 +1,18 @@
 
-angular.module('OsoiteKoostepalvelu', ['ngRoute'])
-	.config(function($routeProvider) {
-		$routeProvider
-		.when('/', {
-			controller:'SearchController',
-			templateUrl:'index.html'
-		})
-		.otherwise({
-			redirectTo:'/'
-		});
-	})
+angular.module('OsoiteKoostepalvelu', []
+//    ,
+//    ['ngRoute']
+    )
+//	.config(function($routeProvider) {
+//		$routeProvider
+//		.when('/', {
+//			controller:'SearchController',
+//			templateUrl:'index.html'
+//		})
+//		.otherwise({
+//			redirectTo:'/'
+//		});
+//	})
 	.factory('Results', function() {
 		return [
 			{firstName: "Dummy", lastName: "Result"}
