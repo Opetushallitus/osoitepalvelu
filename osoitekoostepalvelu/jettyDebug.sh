86c4502b18d24371e597b319d6a912bf2a406050
Tommi Ratamaa <tommi.ratamaa@gmail.com>
2013-12-16 13:02:18 +0200

    Lisätty puuttunut jettyDebug.sh

--- osoitekoostepalvelu/jettyDebug.sh
+++ osoitekoostepalvelu/jettyDebug.sh
@@ -1,2 +1,4 @@
 export MAVEN_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=4000,server=y,suspend=n"
-mvn clean -Djetty.port=9090 jetty:run -Pstartstack
+cd ../osoitepalvelu-ui && mvn3 install \
+	&& cd ../osoitekoostepalvelu \
+	&& mvn clean -Djetty.port=9090 jetty:run -Pstartstack
