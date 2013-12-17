302ebd9f5acda96679c837c3933792875a4840c2
Tommi Ratamaa <tommi.ratamaa@gmail.com>
2013-12-17 16:36:11 +0200

    Lisätty vielä jettyDebug.sh:n viittaus mvn3

--- osoitekoostepalvelu/jettyDebug.sh
+++ osoitekoostepalvelu/jettyDebug.sh
@@ -1,4 +1,4 @@
 export MAVEN_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=4000,server=y,suspend=n"
 cd ../osoitepalvelu-ui && mvn3 install \
 	&& cd ../osoitekoostepalvelu \
-	&& mvn clean -Djetty.port=9090 jetty:run -Pstartstack
+	&& mvn3 clean -Djetty.port=9090 jetty:run -Pstartstack
