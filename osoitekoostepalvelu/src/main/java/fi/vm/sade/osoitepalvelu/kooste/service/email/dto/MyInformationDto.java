01c472747cb5a3bf6e8a0fe5e486027da135b7cd
jsikio <janne.sikio@dicode.fi>
2014-07-16 15:26:06 +0300

    Jos etunimiä useita, otetaan vain ensimmäinen osa sähköpostiosoitteeksi, reply-to tiedoksi käyttäjästä viestintäpalveluintegraatiossa. OPHASPA-877

--- osoitekoostepalvelu/src/main/java/fi/vm/sade/osoitepalvelu/kooste/service/email/dto/MyInformationDto.java
+++ osoitekoostepalvelu/src/main/java/fi/vm/sade/osoitepalvelu/kooste/service/email/dto/MyInformationDto.java
@@ -53,6 +53,10 @@ public class MyInformationDto implements Serializable {
     }
 
     public String getFirstName() {
+        // Jos etunimessä on välilyöntejä, palautetaan sitä ennen oleva osa
+        if( this.firstName != null && firstName.contains(" ") ) {
+            return this.firstName.substring(0, this.firstName.indexOf(" ")).trim();
+        }
         return firstName;
     }
 
